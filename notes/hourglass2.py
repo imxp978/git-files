ccc27992911c59a3ae9c169eca2599d9a6b4c291
chun <imxp978@hotmail.com>
2024-03-11 16:02:32 +0800

    test

--- notes/hourglass2.py
+++ notes/hourglass2.py
@@ -5,4 +5,4 @@ for i in range(n):
     if i <= m-1:
         print( (i)*' ' + (n-i*2)*'*' + (i)*' ' )
     else:
-        print ( int(n-i-1)*' '+ (2*i -3)*'*' + (n-i-1)*' ' )
+        print ( (n-i-1)*' '+ (2*i -n+2)*'*' + (n-i-1)*' ' )
