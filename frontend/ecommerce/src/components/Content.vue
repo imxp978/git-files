a499e6bd28a16b9d6333b2b3d8b6a86fd5144162
imxp978 <imxp978@Hotmail.com>
2024-05-06 00:01:13 +0800

    daily update

--- frontend/ecommerce/src/components/Content.vue
+++ frontend/ecommerce/src/components/Content.vue
@@ -3,12 +3,10 @@
       <div class="container">
       <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button v-for="(item, index) in carouselList" :key="index" type="button" data-bs-target="#carouselExampleCaptions" :data-bs-slide-to="index" :class="{'active': index === 0}" :aria-current="{'true': index===0}" :aria-label="'Slide '+index"></button>
         </div>
         <div class="carousel-inner">
-          <div class="carousel-item"  v-for="(item, index) in carouselList" :key="index" :class="{ 'active': index === 0 }">
+          <div class="carousel-item"  v-for="(item, index) in carouselList" :key="index" :class="{'active': index === 0}">
             <img :src="item.image" class="d-block w-100" alt="...">
             <div class="carousel-caption d-none d-md-block">
               <h5>{{item.title}}</h5>
@@ -39,7 +37,7 @@
             </div>
             <div class="card-body">
               <h5 class="card-title">{{item.title}}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">{{item.content}}</p>
             </div>
           </div>
         </div>
@@ -56,18 +54,19 @@ const carouselList = ref( [
     {id:1, title:"image 1", content:"content1", image:"/images/carousel00.webp" },
     {id:2, title:"image 2", content:"content2", image:"/images/carousel01.webp" },
     {id:3, title:"image 3", content:"content3", image:"/images/carousel02.webp" },
+    {id:4, title:"image 4", content:"content4", image:"/images/carousel01.webp" },
 ])
 
 const categoryList = ref( [
-        {id:1, title:"plate", image:"/images/product_images/001.webp"},
-        {id:2, title:"bowl", image:"/images/product_images/002.jpg"},
-        {id:3, title:"cooker", image:"/images/product_images/003.jpg"},
-        {id:4, title:"pot", image:"/images/product_images/001.webp"},
-        {id:5, title:"good one too", image:"/images/product_images/004.jpg"},
-        {id:6, title:"good one 333", image:"/images/product_images/002.jpg"},
-        {id:7, title:"good one 333", image:"/images/product_images/003.jpg"},
-        {id:8, title:"good one 111", image:"/images/product_images/001.webp"},
-        {id:9, title:"good one too", image:"/images/product_images/002.jpg"},
+        {id:1, title:"Plate", content:"content 1 is very tasty, definitely yum!", image:"/images/product_images/001.webp"},
+        {id:2, title:"Bowl", content:"content 2 is very tasty, definitely yum!", image:"/images/product_images/002.jpg"},
+        {id:3, title:"Pot", content:"content 3 is very tasty, definitely yum!", image:"/images/product_images/003.jpg"},
+        {id:4, title:"Cooker", content:"content 4 is very tasty, definitely yum!", image:"/images/product_images/001.webp"},
+        {id:5, title:"Tableware", content:"content 5 is very tasty, definitely yum!", image:"/images/product_images/004.jpg"},
+        {id:6, title:"Appliance", content:"content 6 is very tasty, definitely yum!", image:"/images/product_images/002.jpg"},
+        {id:7, title:"Good Stuff", content:"content 7 is very tasty, definitely yum!", image:"/images/product_images/003.jpg"},
+        {id:8, title:"good one 111", content:"content 8 is very tasty, definitely yum!", image:"/images/product_images/001.webp"},
+        {id:9, title:"good one too", content:"content 9 is very tasty, definitely yum!", image:"/images/product_images/002.jpg"},
       ]);
 </script>
 
