f754e39e64e427b9be760c77a3947e24dd3e652e
chun <imxp978@hotmail.com>
2024-05-07 14:52:17 +0800

    daily update

--- Lesson-JS/Lesson-11/connections/conn_db.php
+++ Lesson-JS/Lesson-11/connections/conn_db.php
@@ -6,21 +6,3 @@
   $link = new PDO($dsn, $user, $password);
   date_default_timezone_set ("asia/taipei");
 ?>
-
-
-<?php
-  $dsn = "mysql:host=localhost; dbname=pharmacy; charset=utf8";
-  $user = "sales";
-  $password = "ps";
-
-  $link = new PDO($dsn, $user, $password);
-  date_default_timezone_set ('asia/taipei');
-
-?>
-
-
-<?php
-  require_once('connections/conn_db.php');
-?>
-
-<?php (!isset($_SESSION))?session_start():''; ?>
