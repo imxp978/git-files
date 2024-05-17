067f7543c7f65c2f94f6b79ca4e2316243183542
chun <imxp978@hotmail.com>
2024-05-17 14:39:13 +0800

    shit happens

--- frontend/ecommerce_php/index.php
+++ frontend/ecommerce_php/index.php
@@ -342,23 +342,29 @@
     </div>
   </section>
   <section id="reviews" style="width: 100%">
+  <?php $SQLstring="SELECT * FROM reviews";
+  $review = $link->query($SQLstring);
+  echo $review;
+  ?>
     <div class="container text-center mx-auto px-5 py-5">
       <h3 class="my-5 movable slideIn">REVIEWS</h3>
       <div class="row gy-10 gx-5">
-        <div class="card col-6 border-0 border-end border-bottom border-3" v-for="item in reviews">
+      <?php $i = 0;  
+        while($data=$review->fetch()) { ?>
+        <div class="card col-6 border-0 border-end border-bottom border-3">
           <div class="row g-5">
             <div class="review col-md-4 movable slideIn slideInLeft">
               <div class="review-image">
-                <img :src="`/images/user_images/${item.image}`" class="img-fluid rounded-start" alt="..." />
+                <img src="/images/user_images/<?php echo $data['image']; ?>" class="img-fluid rounded-start" alt="..." />
               </div>
             </div>
             <div class="col-md-8">
               <div class="card-body text-start">
-                <h5 class="card-title">review.title</h5>
-                <span v-for="i in item.rate">
+                <h5 class="card-title"><?php echo $data['user']?></h5>
+                <?php for ($j = 0; $j < $data['rate']; $j++) { ?>
                   <i class="fa-solid fa-star"></i>
-                </span>
-                <p class="card-text">review.content</p>
+                <?php } ?>
+                <p class="card-text">review.content<?php echo $data['content']; ?></p>
                 <p class="card-text">
                   <small class="text-muted">Last updated 3 mins ago</small>
                 </p>
@@ -366,6 +372,8 @@
             </div>
           </div>
         </div>
+        <?php $i++; 
+      } ?>
       </div>
       <hr />
     </div>
