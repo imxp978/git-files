f4dac6e7986dea00a69ac787889fcf811d9d4ff0
imxp978 <imxp978@Hotmail.com>
2024-05-20 17:18:21 +0800

    daily update

--- frontend/ecommerce_php/components/footer.php
+++ frontend/ecommerce_php/components/footer.php
@@ -15,7 +15,7 @@
         <div class="col col-12 col-sm-2 text-end">
           <h5>Member</h5>
           <a href="#" data-bs-toggle="modal" data-bs-target="#login">My Account</a><br />
-          <a href="#" data-bs-toggle="modal" data-bs-target="#cart">My Cart</a><br />
+          <a href="./cart.php">My Cart</a><br />
         </div>
         <div class="col col-12 col-sm-2 text-end">
           <h5>Terms & Conditions</h5>
@@ -33,4 +33,4 @@
     </div>
   </section>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <script src="./move.js"></script>
+  <script src="./main.js"></script>
