2a1492e84e5eeba979f804970614d0615b6e2086
imxp978 <103916953+imxp978@users.noreply.github.com>
2024-05-29 00:30:00 +0800

    daily update

--- frontend/ecommerce_php/components/navbar.php
+++ frontend/ecommerce_php/components/navbar.php
@@ -10,7 +10,7 @@
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse bg-light" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="./">Home</a>
